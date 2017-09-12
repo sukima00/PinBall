ef3e07239f4054642e94f88da02465eed9d6956f
筒井　匠 <noltyska0883@gmail.com>
2017-09-12 16:21:07 +0900

    Lesson5課題提出（課題：得点を表示しよう）
    Lesson5で作成したPinBallをもとに作成。

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    // スコアを表示するテキスト
+    private GameObject scoreText;
+
+    // スコアの初期値
+    int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // ScoreTextにスコアを表示
+        this.scoreText.GetComponent<Text>().text = "score : " + score;
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other){
+
+        // タグによって異なる点数を加算する
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+    }
+}
